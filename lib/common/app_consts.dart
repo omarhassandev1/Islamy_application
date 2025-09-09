7da9f3eaff3f7879f12b1c134bb469f87819bfd3
omarhassan168 <oh569678@gmail.com>
2025-09-09 17:15:19 +0300

    update

--- lib/common/app_consts.dart
+++ lib/common/app_consts.dart
@@ -1,8 +1,11 @@
 class AppConsts{
   static const String fontFamily = 'poppins';
+
   static const String quranIcon = 'assets/icons/quran.svg';
   static const String hadithIcon = 'assets/icons/hadith.svg';
   static const String radioIcon = 'assets/icons/radio.svg';
   static const String sebhaIcon = 'assets/icons/sebha.svg';
   static const String timeIcon = 'assets/icons/time.svg';
+
+
 }
